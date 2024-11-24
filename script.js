2ceec3e0d6e427798c38a31d3839ca4e6938ddcc
ShubhamKang <kangshubham82@gmail.com>
2024-11-24 17:09:38 +0530

    add U and D matrix

--- script.js
+++ script.js
@@ -139,3 +139,28 @@ function lmatrix(id){
     
     document.getElementById(id).innerHTML += matrixFormat(L);
 }
+function UUmatrix(uumat){
+    for (let i=0;i<n;i++){
+        r=[]
+        let fac=U[i][i]
+        for ( let j=0;j<n;j++){
+            r.push(U[i][j]/fac)
+        }
+        Uu.push(r)
+    }
+    document.getElementById(uumat).innerHTML+=matrixFormat(Uu);
+}
+
+function Dmatrix(dmat){
+    for (let i=0;i<n;i++){
+        D[i] = [];
+        for (let j=0;j<n;j++){
+            if (i==j)
+                D[i][j] = U[i][i];
+            else
+                D[i][j] = 0;
+        }
+    }
+
+    document.getElementById(dmat).innerHTML+=matrixFormat(D);
+}
